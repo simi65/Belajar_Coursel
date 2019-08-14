c3d604327ed1c48457a9cda49f210d3f1ce56ea8
MISBAH <misbah@MacBook-Pro-1.local>
2019-08-14 14:35:34 +0700

    Initial commit

---
+++ app/src/main/java/com/miss/belajar_coursel/MainActivity.java
@@ -0,0 +1,35 @@
+package com.miss.belajar_coursel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    CarouselView carouselView;
+    int[] sampleImage = {R.drawable.misbaha, R.drawable.misbahb , R.drawable.misbahs};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImage.length);
+        carouselView.setImageListener(imageListener);
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImage[position]);
+
+        }
+    };
+}
